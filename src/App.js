c45d319d69f64ee095c252d47336ee41efefd102
rohansinghwow <rohansinghwow@gmail.com>
2021-12-16 13:38:17 +0530

    adding logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [isDark, setIsDark] = React.useState('false')
+    function trigger(){
+        setIsDark(prevState=>!prevState)
+        console.log("pressed")
+    }
+    
+    return (
+        <div className="container">
+            <Navbar darkMode={isDark} func={trigger}/>
+            <Main darkMode={isDark}/>
+        </div>
+    )
 }
-
-export default App;
